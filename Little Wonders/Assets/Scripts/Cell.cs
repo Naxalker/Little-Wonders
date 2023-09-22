3f744c087d1066160149147d94f06537ea63f233
Ilya Savenkov <s4venkov1lya@gmail.com>
2023-09-23 00:48:04 +0300

    Почти всё сделали!!!

--- Little Wonders/Assets/Scripts/Cell.cs
+++ Little Wonders/Assets/Scripts/Cell.cs
@@ -6,17 +6,43 @@ public class Cell : MonoBehaviour
 {
     [SerializeField] private Color baseColor, offsetColor;
     [SerializeField] GameObject highlight;
+    [SerializeField] GridBehavior grid;
     
     private SpriteRenderer spriteRenderer;
+    private Vector2Int coordinates;
+    private bool isOffset;
+
+    [HideInInspector] public bool isExplored = false;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        grid = GetComponentInParent<GridBehavior>();
     }
 
-    public void Init(bool isOffset)
+    public void Init(bool _isOffset, bool _isExplored, Vector2Int _coords)
     {
-        spriteRenderer.color = isOffset ? offsetColor : baseColor;
+        isOffset = _isOffset;
+        coordinates = _coords;
+        isExplored = _isExplored;
+        
+        if (!isExplored)
+        {
+            spriteRenderer.color = new Color(0f,0f,0f);
+        }
+        else 
+        {
+            spriteRenderer.color = isOffset ? offsetColor : baseColor;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (IsNextToNeighbour())
+        {
+            isExplored = true;
+            spriteRenderer.color = isOffset ? offsetColor : baseColor;
+        }
     }
 
     private void OnMouseEnter()
@@ -27,5 +53,15 @@ public class Cell : MonoBehaviour
     private void OnMouseExit()
     {
         highlight.SetActive(false);
+    }
+
+    private bool IsNextToNeighbour()
+    {
+        if (grid.cells[(int)Mathf.Clamp(coordinates.x - 1, 0, grid.size.x), coordinates.y].isExplored) return true;
+        if (grid.cells[(int)Mathf.Clamp(coordinates.x + 1, 0, grid.size.x), coordinates.y].isExplored) return true;
+        if (grid.cells[coordinates.x, (int)Mathf.Clamp(coordinates.y - 1, 0, grid.size.y)].isExplored) return true;
+        if (grid.cells[coordinates.x, (int)Mathf.Clamp(coordinates.y + 1, 0, grid.size.y)].isExplored) return true;
+
+        return false;
     }
 }
